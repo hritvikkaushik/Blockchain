b337c530efee2a9c1cfe8ca47b8e3c6ceff50aef
Hritvik <hritvikkaushik@gmail.com>
2020-08-01 13:07:42 +0530

    Methods for hash, last_block and init

--- Blockchain.py
+++ Blockchain.py
@@ -1,11 +1,33 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        #Create and add new block to chain
-        pass
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create new block
+        :param proof: <int> From Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain)+1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -14,7 +36,7 @@ class Blockchain(object):
         :param amount: <int> Amount
         :return: <int> Index of block that will hold this transaction. 
         """
-        
+
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -25,10 +47,15 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-        #Hash a block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        #Ensure that dictionary is ordered else hashes will be iniconsistent
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        #Return last block in the chain
-        pass
+        return self.chain[-1]
